1aba37b4729ace3b0437cd28b4d1d0a6d89f73a9
zana-abdollahpour <zana.ap4@gmail.com>
2024-02-19 15:55:12 +0330

    set up NextAuth

---
+++ src/auth.ts
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import Github from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import { db } from "@/db";
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET)
+  throw new Error("Missing Github Credentials");
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    Github({
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+    }),
+  ],
+});
