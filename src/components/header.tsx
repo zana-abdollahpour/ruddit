3f775f5faf9786e3fe992a44fe06b76bb03ba74b
zana-abdollahpour <zana.ap4@gmail.com>
2024-02-20 21:34:31 +0330

    implement sign in / sign out functionality in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,9 +8,13 @@ import {
   Input,
   Button,
   Avatar,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
 } from "@nextui-org/react";
 
 import { auth } from "@/auth";
+import * as actions from "@/actions";
 
 export default async function Header() {
   const session = await auth();
@@ -18,23 +22,38 @@ export default async function Header() {
   let authContent: React.ReactNode;
   if (session?.user) {
     authContent = (
-      <Avatar
-        src={session.user.image || ""}
-        alt={session.user.name || "user"}
-      />
+      <Popover placement="left">
+        <PopoverTrigger>
+          <Avatar
+            src={session.user.image || ""}
+            alt={session.user.name || "user"}
+          />
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="p-4">
+            <form action={actions.signOut}>
+              <Button type="submit">Sign Out</Button>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
     );
   } else {
     authContent = (
       <>
         <NavbarItem>
-          <Button type="submit" color="secondary" variant="bordered">
-            Sign In
-          </Button>
+          <form action={actions.signIn}>
+            <Button type="submit" color="secondary" variant="bordered">
+              Sign In
+            </Button>
+          </form>
         </NavbarItem>
         <NavbarItem>
-          <Button type="submit" color="primary" variant="flat">
-            Sign Up
-          </Button>
+          <form action={actions.signIn}>
+            <Button type="submit" color="primary" variant="flat">
+              Sign Up
+            </Button>
+          </form>
         </NavbarItem>
       </>
     );
