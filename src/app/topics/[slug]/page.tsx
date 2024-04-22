03e612bdca0f83f26d26cb4a0cf36319f7b7c2ba
zana-abdollahpour <zana.ap4@gmail.com>
2024-04-22 19:16:13 +0330

    scaffold  the topic show page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,17 @@
-export default function TopicShowPage() {
-  return <div>TopicShowPage</div>;
+interface TopicShowPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function TopicShowPage({ params }: TopicShowPageProps) {
+  const { slug } = params;
+
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="mb-2 text-2xl font-bold">{slug}</h1>
+      </div>
+    </div>
+  );
 }
