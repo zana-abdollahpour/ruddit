1f97bf95b7b7c55ea0b0ce5550a2aa2384d6ec87
zana-abdollahpour <zana.ap4@gmail.com>
2024-04-22 19:11:06 +0330

    show topics list on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import TopicCreateForm from "@/components/topics/topic-create-form";
+import TopicList from "@/components/topics/topic-list";
+import { Divider } from "@nextui-org/react";
 
 export default function HomePage() {
   return (
@@ -6,8 +8,11 @@ export default function HomePage() {
       <div className="col-span-3">
         <h1 className="m-2 text-xl">Top Posts</h1>
       </div>
-      <div>
+      <div className="border px-2 py-3 shadow">
         <TopicCreateForm />
+        <Divider className="my-2" />
+        <h3 className="pb-2 text-lg">Topics</h3>
+        <TopicList />
       </div>
     </div>
   );
